5ac34d2d9156f00245c4d0d15aa58523afc7aedf
Durrotun Nafisah <125974370+Dnafisah@users.noreply.github.com>
2023-05-12 15:14:45 +0700

    Delete Restaurant-Apps-Submission directory

--- Restaurant-Apps-Submission/src/scripts/globals/api-endpoint.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  DAFTAR: `${CONFIG.BASE_URL}list`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
-};
-
-export default API_ENDPOINT;
